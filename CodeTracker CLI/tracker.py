f9abe113138548e804b8333445fb6f1b0bac73c0
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-07-01 13:57:14 +0500

    init json database file layout

---
+++ CodeTracker CLI/tracker.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+DB_FILE = "data.json"
+
+def load_data():
+  if not os.path.exists(DB_FILE):
+    return {"tasks": []}
+  with open(DB_FILE, "r") as f:
+    return json.load(f)
+
+def save_data(data):
+  with open(DB_FILE, "w") as f:
+    json.jump(data, f, indent=4)
